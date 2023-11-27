93c8bf3ee3d99209689e990f7f5e434faeda73b7
gabr99el <wesllengabr11el2003x@gmail.com>
2023-11-27 10:11:50 -0300

    migration2

---
+++ ApiEmpresa3d/ApiEmpresa3d/Migrations/20231127131103_migração2.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiEmpresa3d.Migrations
+{
+    /// <inheritdoc />
+    public partial class migração2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Material",
+                table: "Projeto",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Material",
+                table: "Projeto");
+        }
+    }
+}
